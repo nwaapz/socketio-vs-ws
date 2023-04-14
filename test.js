6dce5747e87b40dbce2ca7eb1bd76561ddd6eb53
hosein abdl <53816663+nwaapz@users.noreply.github.com>
2023-04-14 21:42:11 +0430

    io

---
+++ test.js
@@ -0,0 +1,28 @@
+const port = 8080;
+const io = require('socket.io')();
+io.use((socket, next) => {
+    if (socket.handshake.query.token === "UNITY") {
+        next();
+    } else {
+        next(new Error("Authentication error"));
+    }
+});
+
+io.on('connection', socket => {
+  socket.emit('connection', {date: new Date().getTime(), data: "Hello Unity"})
+
+  socket.on('hello', (data) => {
+    socket.emit('hello', {date: new Date().getTime(), data: data});
+  });
+
+  socket.on('spin', (data) => {
+    socket.emit('spin', {date: new Date().getTime()});
+  });
+
+  socket.on('class', (data) => {
+    socket.emit('class', {date: new Date().getTime(), data: data});
+  });
+});
+
+io.listen(port);
+console.log('listening on *:' + port);
